269fdd392836feb6d2403dcd1de9a2cdd812ccd6
Katie <adlerk8@gmail.com>
2020-10-21 17:28:32 -0700

    ready for review

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -11,17 +11,25 @@ class App extends Component {
     }
   }
 
-  clickAlert () {
-    return alert(`username: ${this.username} password: ${this.password}`);
+  updatePassword(e) {
+    this.setState({
+      password: e
+    });
+  }
+
+  updateUsername(e) {
+    this.setState({
+      username: e
+    });
   }
 
   render() {
 
     return (
       <div>
-        <input placeholder='username'/>
-        <input placeholder='password'/>
-        <button onClick={this.clickAlert}>Login</button>
+        <input placeholder='username' onChange={(e) => this.updateUsername(e.target.value)}/>
+        <input placeholder='password' onChange={(e) => this.updatePassword(e.target.value)}/>
+        <button onClick={() => window.alert(`username: ${this.state.username} password: ${this.state.password}`)}>Login</button>
       </div>
     );
   } 
